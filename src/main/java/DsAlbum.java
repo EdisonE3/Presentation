f501a6f4b99620b0e6d1bbcdd3facd04c424bf16
edison <735318617@qq.com>
2021-04-19 09:09:12 +0800

    add some get method

--- src/main/java/DsAlbum.java
+++ src/main/java/DsAlbum.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
 
 public class DsAlbum {
-    public ArrayList<Album> albums;
+    public static ArrayList<Album> albums = new ArrayList<>();
     public ArrayList<Album> albumsCache;
 
     public DsAlbum() {
-        this.albums = new ArrayList<>();
         this.albumsCache = new ArrayList<>();
     }
 
     public void addAlbums(int rowId, String title, String artist, boolean isClassical, String composer) {
         if (albumsCache == null) return;
         this.albumsCache.add(new Album(rowId, title, artist, isClassical, composer));
+    }
+
+    public void acceptChanges(){
+        for (Album album : albumsCache) {
+            albums.set(album.rowId - 1, album);
+        }
+        albumsCache.clear();
     }
 }
--- src/main/java/PresentationMod.java
+++ src/main/java/PresentationMod.java
@@ -1,15 +1,82 @@
 public class PresentationMod {
-    boolean isClassical;
-    String composer;
-    Album album;
+    private DsAlbum data;
+    private int selectedAlbumNumber;
+    private Album selectedAlbum;
 
-    public boolean shouldEnableComposer(){
-        return this.isClassical;
-    }
-
-    public static DsAlbum AlbumDataSet(){
+    public static DsAlbum AlbumDataSet() {
         DsAlbum result = new DsAlbum();
-        
+        result.addAlbums(1, "HQ", "Roy Harper", false, null);
+        result.addAlbums(2, "The Rough Dancer and Cyclical Night", "Astor Piazzola", false, null);
+        result.addAlbums(3, "The Black Light", "Calexico", false, null);
+        result.addAlbums(4, "Symphony No.5", "CBSO", true, "Sibelius");
+        result.acceptChanges();
         return result;
     }
+
+    public PresentationMod(DsAlbum data) {
+        this.data = data;
+        this.selectedAlbumNumber = 1;
+        this.selectedAlbum = DsAlbum.albums.get(0);
+    }
+
+    public DsAlbum getData() {
+        return data;
+    }
+
+    public void setData(DsAlbum data) {
+        this.data = data;
+    }
+
+    public int getSelectedAlbumNumber() {
+        return selectedAlbumNumber;
+    }
+
+    public void setSelectedAlbumNumber(int selectedAlbumNumber) {
+        this.selectedAlbumNumber = selectedAlbumNumber;
+        this.selectedAlbum = DsAlbum.albums.get(selectedAlbumNumber - 1);
+    }
+
+    public String getTitle() {
+        return selectedAlbum.title;
+    }
+
+    public void setTitle(String value) {
+        selectedAlbum.title = value;
+    }
+
+    public String getArtist() {
+        return selectedAlbum.artist;
+    }
+
+    public void setArtist(String value) {
+        selectedAlbum.artist = value;
+    }
+
+    public boolean getIsClassical() {
+        return selectedAlbum.isClassical;
+    }
+
+    public void setIsClassical(boolean value) {
+        selectedAlbum.isClassical = value;
+    }
+
+    public String getComposer() {
+        return selectedAlbum.isClassical ? selectedAlbum.composer : "";
+    }
+
+    public void setComposer(String value) {
+        if (selectedAlbum.isClassical) {
+            selectedAlbum.composer = value;
+        }
+    }
+
+    public String getFormTitle(){
+        return "Album: " + getTitle();
+    }
+
+    public boolean shouldEnableComposer() {
+        return selectedAlbum.isClassical;
+    }
+
+
 }
